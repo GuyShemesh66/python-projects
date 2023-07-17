b3cde7873c95f74a53903ddfecf3f0be971768dc
GuyShemesh66 <139698427+GuyShemesh66@users.noreply.github.com>
2023-07-17 16:06:05 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, Extension
-
-# Define the extension module
-kmeans_extension = Extension('mykmeanssp',sources=['kmeansmodule.c'])
-
-# Setup configuration
-setup(name='mykmeanssp',version='1.0',description='Extension module for K-means algorithm',ext_modules=[kmeans_extension])
